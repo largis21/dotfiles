05fb35778d4f9934a3d5b5cd6df6676efda1c73d
Lars-Ivar Giskegjerde <lars-ivar.giskegjerde@twoday.com>
2024-08-13 22:12:45 +0200

    Added oh my zsh to zsh install script

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -1,2 +1,3 @@
 sudo apt install zsh -y
-chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+chsh -s "/bin/zsh" "$USER"
