967716d555487d48176ead7681d1bbe622169e57
Lars-Ivar Giskegjerde <lars-ivar.giskegjerde@twoday.com>
2024-08-13 22:38:34 +0200

    Fix oh my zsh install to not override .zshrc

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -1,4 +1,8 @@
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s "/bin/zsh" "$USER"
+rm "$HOME/.zshrc"
+cp "../.zshrc" "$HOME/.zshrc"
 zsh
