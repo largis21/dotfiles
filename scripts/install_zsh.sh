9ac169f7a2f0ba895d38f25f02b68b7ff814bcd5
Lars-Ivar Giskegjerde <lars-ivar.giskegjerde@twoday.com>
2024-08-13 22:42:14 +0200

    Fix

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -1,4 +1,6 @@
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+#!/bin/bash
+
+cd "$(dirname "$0")"
 
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
