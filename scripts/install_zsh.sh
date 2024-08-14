ac4bd00491c1d86861dfc11ca5a560e69b81e6be
Lars-Ivar Giskegjerde <lars-ivar.giskegjerde@twoday.com>
2024-08-14 08:47:24 +0200

    Fixed zsh install script

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
-
+# Install Starship
 curl -sS https://starship.rs/install.sh | sh
 
+# Install zsh
 sudo apt install zsh -y
+
+# Remove existing oh-my-zsh
+rm -rf "$HOME/.oh-my-zsh"
+
+# Run oh-my-zsh install script
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Set zsh to default shell
 chsh -s "/bin/zsh" "$USER"
 
-if [ -f "$HOME/.zshrc" ]; then
-  rm "$HOME/.zshrc"
-fi
+# Remove oh my zsh default config and replace with my own ../.zshrc
+rm "$HOME/.zshrc"
+ln -s "$(pwd)/../.zshrc" "$HOME/.zshrc"
 
-ln -s "../.zshrc" "$HOME/.zshrc"
+# Run zsh
 zsh
